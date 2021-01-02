7e7e99c63bb3bfccfd9c48862a9873600c56028b
jasmaa <jasonmaa3955@gmail.com>
2021-01-02 11:58:10 -0500

    Prepare for deployment. Change work 9.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Blackbird Gallery
+# Blackbird Viewing Gallery
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -3,7 +3,7 @@ export default function Welcome({ isEntering, enter }) {
     <div className={isEntering ? 'animate__animated animate__fadeOut' : 'animate__animated animate__fadeIn'}>
       <div className="container mx-auto px-5">
         <div className="flex flex-col items-center">
-          <p className="text-6xl mt-14 mb-20">Blackbird Gallery</p>
+          <p className="text-6xl mt-14 mb-20">Blackbird Viewing Gallery</p>
           <button className="rounded border-2 border-gray-500 text-gray-500 p-2" onClick={enter}>Enter</button>
         </div>
       </div>
