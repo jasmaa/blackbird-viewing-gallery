349604d38a0b8eb5cb75cc911dfcade3c210760f
jasmaa <jasonmaa3955@gmail.com>
2021-01-02 12:13:11 -0500

    Change font

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -5,26 +5,31 @@ import { works } from './works';
  */
 export default function Gallery() {
   return (
-    <div className="container mx-auto px-5">
-      <div className="flex flex-col items-center">
-        <div className="grid grid-cols-1 gap-4 animate__animated animate__fadeIn">
-          {works.map(({ title, medium, subtitle, url }) => (
-            <div key={title} className="my-12">
-              <img src={url} className="shadow-lg rounded-lg w-full" />
-              <div className="text-gray-500 mt-3">
-                <p className="text-md font-bold text-black">{title}</p>
-                <p className="text-sm">{subtitle}</p>
-                <p className="text-sm">{medium}</p>
-              </div>
-            </div>
-          ))}
+    <div className="animate__animated animate__fadeIn">
+      <div className="container mx-auto px-5">
+        <div className="flex flex-col items-center">
 
-          <div className="my-12">
-            <iframe className="shadow-lg rounded-lg w-full" src="index.html" height="300"></iframe>
-            <div className="text-gray-500 mt-3">
-              <p className="text-md font-bold text-black">Blackbird Gallery</p>
-              <p className="text-sm">American, 2021</p>
-              <p className="text-sm">Elements in DOM</p>
+          <p className="align-center text-4xl mt-14 mb-10">Blackbird Viewing Gallery</p>
+
+          <div className="grid grid-cols-1 gap-4">
+            {works.map(({ title, medium, subtitle, url }) => (
+              <div key={title} className="my-12">
+                <img src={url} className="shadow-lg rounded-lg w-full" />
+                <div className="text-gray-500 mt-3">
+                  <p className="text-md font-bold text-black">{title}</p>
+                  <p className="text-sm">{subtitle}</p>
+                  <p className="text-sm">{medium}</p>
+                </div>
+              </div>
+            ))}
+
+            <div className="my-12">
+              <iframe className="shadow-lg rounded-lg w-full" src="index.html" height="300"></iframe>
+              <div className="text-gray-500 mt-3">
+                <p className="text-md font-bold text-black">Blackbird Viewing Gallery</p>
+                <p className="text-sm">American, 2021</p>
+                <p className="text-sm">Elements in DOM</p>
+              </div>
             </div>
           </div>
         </div>
--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -3,8 +3,8 @@ export default function Welcome({ isEntering, enter }) {
     <div className={isEntering ? 'animate__animated animate__fadeOut' : 'animate__animated animate__fadeIn'}>
       <div className="container mx-auto px-5">
         <div className="flex flex-col items-center">
-          <p className="text-6xl mt-14 mb-20">Blackbird Viewing Gallery</p>
-          <button className="rounded border-2 border-gray-500 text-gray-500 p-2" onClick={enter}>Enter</button>
+          <p className="align-center text-4xl mt-14 mb-20">Blackbird Viewing Gallery</p>
+          <button className="text-gray-500 focus:outline-none p-2" onClick={enter}>Enter</button>
         </div>
       </div>
     </div>
